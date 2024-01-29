a1e8806dd5daa35dc3ea54ac5599bf582d3b527b
Adryan Oliveira <122647064+adryanzx@users.noreply.github.com>
2024-01-29 15:20:08 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,47 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Quantos que é 2+2?",
+    answer: "4",
+    options: [
+      "3",
+      "7",
+      "4",
+      "2"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quantos que é 5x5?",
+    answer: "25",
+    options: [
+      "35",
+      "20",
+      "25",
+      "30"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Quantas pata tem um cachorro?",
+    answer: "4",
+    options: [
+      "4",
+      "2",
+      "3",
+      "5"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Quantos que é 2x2x2?",
+    answer: "8",
+    options: [
+      "7",
+      "9",
+      "8",
+      "5"
+    ]
+  },
+   
+];
